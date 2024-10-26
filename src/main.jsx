83de6f69f67870e29b4201c6b94e48e1c4720226
Cawan Carvalho <cawancarvalho3@gmail.com>
2024-10-26 16:55:27 -0300

    feat: add eslint and prettier config
    Added prettier and eslint configuration for code quality and formatting

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,4 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <App /> */}
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<StrictMode></StrictMode>)
